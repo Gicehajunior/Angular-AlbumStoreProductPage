9cd797cdc42893290397329ebbdd9f077fb0defa
Bernard <gicehajunior76@gmail.com>
2020-09-29 02:00:16 +0300

    constructor refactoring on the parameters

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -7,7 +7,7 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-description.component.css']
 })
 export class ProductDescriptionComponent implements OnInit {
-  albumInfo;
+  public albumInfo;
   constructor(private _productService: ProductService) {}
 
   ngOnInit() {
