72132eb1c395eb313d0e26b209eb9819726bcf8f
Bernard <gicehajunior76@gmail.com>
2020-10-16 23:08:52 +0300

    refactor display list of products on product list component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -8,14 +8,15 @@ import { Product } from './product';
 @Injectable()
 export class ProductService {
   private _albumUrl = '../assets/album.json';
+  private _productsUrl = '../assets/prodcts.json';
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number): Observable<Album>{
+  getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 
-  getProducts(): Observable<Product[]>{
-    return this._http.get(this._albumUrl).map(response => <Product[]>response.json())
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map(response => <Product[]>response.json())
   }
 }
